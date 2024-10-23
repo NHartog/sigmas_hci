d9f4614193791407c60c1fdef18f064673bd03ed
SuperOwl52 <nicholas52.hartog@gmail.com>
2024-10-23 01:04:28 -0400

    Finished Rough Draft of application page

--- src/app/[userType]/(nonmanager)/application/page.tsx
+++ src/app/[userType]/(nonmanager)/application/page.tsx
@@ -1,17 +1,24 @@
 "use server";
 
 import {Box, Typography} from '@mui/material'
+import Stepper from '@mui/material/Stepper'
+import Step from '@mui/material/Step'
+import StepLabel from '@mui/material/StepLabel'
+import ApplicationStepper from '@/Component/application'
+import {Stack} from "@mui/system";
+
 
 export default async function LandingPage() {
 
     return (
-        <Box style={{ padding: "20px", textAlign: "center" }}>
+        <Stack style={{ padding: "20px", textAlign: "center", justifyContent: "center" }}>
             <Typography variant="h2" component="h1" gutterBottom>
                 This is the Application Page
             </Typography>
             <Typography variant="body1" paragraph>
                 Here we will add Application
             </Typography>
-        </Box>
+            <ApplicationStepper />
+        </Stack>
     );
 }
