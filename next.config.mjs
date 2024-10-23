6452d608fca1332d514c8df89417097569c47277
Sebastian Paulis <sebastian4paulis@gmail.com>
2024-10-22 21:21:59 -0400

    feat: Create about section on home page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.gainesville.com',
+        port: '',
+      }
+    ]
+  }
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,26 @@
 "use server";
 
-import {Box, Button, Typography} from '@mui/material'
+import {Box, Button, Container, Stack, Typography} from '@mui/material'
 import {headers} from "next/headers";
+import About from "@/Component/about"
+import Image from 'next/image';
 
 export default async function LandingPage() {
     console.log("meow")
   return (
-      <Box style={{ padding: "20px", textAlign: "center" }}>
-        <Typography variant="h2" component="h1" color= "black"gutterBottom>
+      <Box style={{ padding: 20, textAlign: "center" }}>
+        <Typography variant="h2" color= "black"gutterBottom>
           Welcome to the UF TA System
         </Typography>
-        <Typography variant="body1" color = "black" paragraph>
-          Manage your TA applications, roles, and settings all in one place!
-        </Typography>
-        <Button variant="contained" color="primary" href="/login">
+        <Container maxWidth="md" sx={{position: "relative", aspectRatio: '1.5', mb: 2}}>
+          <Image
+            src="https://www.gainesville.com/gcdn/authoring/2019/01/15/NTGS/ghows-LK-7f853cad-7096-0edd-e053-0100007fd056-63268455.jpeg"
+            alt="Picture of Teaching Assistant"
+            fill
+          />
+        </Container>
+        <About/>
+        <Button sx={{mt: 3}} variant="contained" color="primary" href="/login">
           Login
         </Button>
       </Box>
