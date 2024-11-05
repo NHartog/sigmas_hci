25978328805391de2ef493f55724f7f42b4eb0a9
SuperOwl52 <nicholas52.hartog@gmail.com>
2024-11-05 17:31:57 -0500

    Added a paper to the Student Home page so we can see their assigned courses

--- src/app/[userType]/(nonmanager)/ta/home/page.tsx
+++ src/app/[userType]/(nonmanager)/ta/home/page.tsx
@@ -1,13 +1,25 @@
 "use server";
 
-import { Button, Container, Paper, Stack, Typography } from '@mui/material'
+import {
+    Container,
+    Paper,
+    Stack,
+    Typography,
+    Divider,
+    Button
+} from '@mui/material';
 import WithdrawButton from './withdrawButton';
 import { getUserData } from '@/actions/application';
+import { getCourses } from "@/actions/professor";
 
 export default async function LandingPage() {
+    const user = await getUserData();
+    const courses = await getCourses();
 
-    const user = await getUserData()
-    var options: any = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
+    // Filter courses where the logged-in student is part of `assignedTas`
+    const studentCourses = courses.filter(course => course.assignedTas.includes(user.name));
+
+    const options: any = {weekday: "long", year: "numeric", month: "long", day: "numeric"};
 
     return (
         <Container maxWidth="md">
@@ -31,10 +43,68 @@ export default async function LandingPage() {
                     </Stack>
                 </Stack>
                 <Stack direction="row" justifyContent="space-between" sx={{ pt: 2 }}>
-                    <WithdrawButton/>
+                    <WithdrawButton />
                     <Button href='/student/application' variant="outlined">Edit Application</Button>
                 </Stack>
             </Paper>
+
+            {/* Display Courses the Student is Part of */}
+            {studentCourses.length > 0 && (
+                <Paper elevation={5} style={{ marginTop: "20px", padding: "20px" }}>
+                    <Typography variant="h4" align="center" gutterBottom>Assigned Courses</Typography>
+                    <Stack spacing={2} sx={{ p: 2 }}>
+                        {studentCourses.map((course) => (
+                            <Stack key={course.prefix} spacing={2} divider={<Divider orientation="horizontal" flexItem />}>
+                                <Typography variant="h5" align="center">{course.prefix}: {course.title}</Typography>
+
+                                <Stack direction="row" alignItems="center" spacing={2}>
+                                    <Typography variant="h6" sx={{ width: "50%" }}>TA Hours:</Typography>
+                                    <Typography sx={{ width: "50%" }}>{course.numTaHours}</Typography>
+                                </Stack>
+
+                                <Stack direction="row" alignItems="center" spacing={2}>
+                                    <Typography variant="h6" sx={{ width: "50%" }}>Current Enrollment:</Typography>
+                                    <Typography sx={{ width: "50%" }}>{course.currentEnrollment} / {course.maxEnrollment}</Typography>
+                                </Stack>
+
+                                <Stack direction="row" alignItems="center" spacing={2}>
+                                    <Typography variant="h6" sx={{ width: "50%" }}>Sections:</Typography>
+                                    <Typography sx={{ width: "50%" }}>{course.sections}</Typography>
+                                </Stack>
+
+                                {/* Assigned TAs */}
+                                <Stack direction="row" alignItems="center" spacing={2}>
+                                    <Typography variant="h6" sx={{ width: "50%" }}>Assigned TAs:</Typography>
+                                    <Stack direction="row" spacing={1} sx={{ width: "50%" }}>
+                                        {course.assignedTas.length > 0 ? (
+                                            course.assignedTas.map((ta : any, index : any) => (
+                                                <Typography key={index}>{ta}{index < course.assignedTas.length - 1 ? ',' : ''}</Typography>
+                                            ))
+                                        ) : (
+                                            <Typography>No TAs Assigned</Typography>
+                                        )}
+                                    </Stack>
+                                </Stack>
+
+                                {/* Professors */}
+                                <Stack direction="row" alignItems="center" spacing={2}>
+                                    <Typography variant="h6" sx={{ width: "50%" }}>Professors:</Typography>
+                                    <Stack direction="row" spacing={1} sx={{ width: "50%" }}>
+                                        {course.professors.length > 0 ? (
+                                            course.professors.map((professor : any, index : any) => (
+                                                <Typography key={index}>{professor}{index < course.professors.length - 1 ? ',' : ''}</Typography>
+                                            ))
+                                        ) : (
+                                            <Typography>No Professors Assigned</Typography>
+                                        )}
+                                    </Stack>
+                                </Stack>
+                            </Stack>
+                        ))}
+                    </Stack>
+                </Paper>
+
+            )}
         </Container>
     );
 }
