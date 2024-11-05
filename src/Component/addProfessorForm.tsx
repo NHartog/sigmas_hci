0c8f99ff1ed71016e7a7efbaa2253acc71284acb
Sebastian Paulis <sebastian4paulis@gmail.com>
2024-11-04 22:01:53 -0500

    fix: Updating UI to look better again

--- src/Component/addProfessorForm.tsx
+++ src/Component/addProfessorForm.tsx
@@ -4,28 +4,21 @@ import React, { useState } from 'react';
 import {
     Box,
     Button,
-    ButtonGroup,
-    Card,
     List,
     ListItem,
     ListItemText,
     TextField,
     Typography,
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableRow,
     Paper,
+    DialogContent, Dialog, DialogTitle,
     Stack,
-    DialogContent, Dialog, DialogTitle
+    Divider
 } from '@mui/material';
-import Link from "next/link";
-import { postProf, getManagerCourses } from '@/actions/manager';
+import { postProf } from '@/actions/manager';
 
-const AddProfessorForm = ({open, onClose, all_Courses}) => {
+const AddProfessorForm = ({ open, onClose, all_Courses }: any) => {
 
-    const allPrefixes = all_Courses.map(obj => obj.Prefix); 
+    const allPrefixes = all_Courses.map((obj: any) => obj.Prefix);
     console.log(allPrefixes)
     const [formData, setFormData] = useState({
         name: '',
@@ -34,177 +27,99 @@ const AddProfessorForm = ({open, onClose, all_Courses}) => {
         course: '',
         username: '',
         password: '',
-      });
+    });
 
     const [filteredItems, setFilteredItems] = useState([])
-    
-    const handleFilteredItems = (value) => {
-        if(value === '' || allPrefixes.includes(value)){
+
+    const handleFilteredItems = (value: any) => {
+        if (value === '' || allPrefixes.includes(value)) {
             setFilteredItems([])
         }
-        else{
-            const matches = allPrefixes.filter(item => item.toLowerCase().includes(value.toLowerCase()))
+        else {
+            const matches = allPrefixes.filter((item: any) => item.toLowerCase().includes(value.toLowerCase()))
             console.log(matches, "     ", value)
             setFilteredItems(matches)
         }
     };
 
-    const handleFormData = (e) => {
-        
+    const handleFormData = (e: any) => {
+
         const { name, value } = e.target;
         setFormData((prevData) => ({
-        ...prevData,
-        [name]: value,  // Dynamically update based on input name
+            ...prevData,
+            [name]: value,  // Dynamically update based on input name
         }));
 
-        if(name === 'course'){
+        if (name === 'course') {
             handleFilteredItems(value)
         }
-        
+
     };
 
-    const handleItemClick = (item) => {
+    const handleItemClick = (item: any) => {
         setFormData((prevData) => ({
             ...prevData,
             ['course']: item
-            }));
-            setFilteredItems([]);
+        }));
+        setFilteredItems([]);
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = (e: any) => {
         e.preventDefault();
         console.log(formData);  // Log or send data to an API
-        const sentData = {...formData, courses: [formData.course]};
+        const sentData = { ...formData, courses: [formData.course] };
         postProf(sentData);
         onClose();
-      };
-      
-    return(
-    <Dialog open={open} onClose={onClose} fullWidth maxWidth="max" scroll="paper">
-        <DialogTitle>Add Professor</DialogTitle>
-        <DialogContent>
-            <Box style={{ padding: "20px", textAlign: "center", gridTemplateColumns: '1fr 1fr'}}>
-                <Box sx={{backgroundColor: "rgba(255, 127, 50, 1)",
-                        borderTopLeftRadius: "15px", borderTopRightRadius: "15px",
-                        padding: "20px"
-                    }}>
-                    <Typography variant="h3">
-                        Add Professor
-                    </Typography>
+    };
+
+    function getInput(label: string, name: string) {
+        return (
+            <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+                <Typography sx={{ textAlign: "right", fontSize: "150%", width: "50%" }}>
+                    {label}:
+                </Typography>
+                <Typography sx={{ textAlign: "left", marginLeft: "10%", fontSize: "150%", width: "50%" }}>
+                    <TextField
+                        name={name}
+                        label={label}
+                        variant="outlined"
+                        value={(formData as any)[name]}
+                        onChange={(e) => handleFormData(e)}
+                        sx={{ width: "90%", marginTop: "10px" }}
+                    />
+                </Typography>
+            </Box>
+        )
+    }
+
+    return (
+        <Dialog open={open} onClose={onClose} fullWidth scroll="paper">
+            <DialogContent sx={{ p: 0 }}>
+                <Box style={{ textAlign: "center", gridTemplateColumns: '1fr 1fr' }}>
+                    <Box sx={{ padding: "20px" }}>
+                        <Typography variant="h5">
+                            Add Professor
+                        </Typography>
+                    </Box>
                 </Box>
-            </Box>
-            <form onSubmit={handleSubmit}>
-            <Box sx={{display: "flex", flexDirection: "row", alignItems: "center"}}>
-                <Typography sx={{textAlign: "right", fontSize: "150%", width: "50%"}}>
-                    Name:
-                </Typography>
-                <Typography sx={{textAlign: "left", marginLeft: "10%", fontSize: "150%", width: "50%"}}>
-                    <TextField
-                        name="name"
-                        label="Name"
-                        variant="outlined"
-                        value={formData.name}
-                        onChange={(e) => handleFormData(e)}
-                        sx={{ width: "90%", marginTop: "10px" }}
-                    />
-                </Typography>
-            </Box>
-            <Box sx={{display: "flex", flexDirection: "row", alignItems: "center"}}>
-                <Typography sx={{textAlign: "right", fontSize: "150%", width: "50%"}}>
-                    Email:
-                </Typography>
-                <Typography sx={{textAlign: "left", marginLeft: "10%", fontSize: "150%", width: "50%"}}>
-                    <TextField
-                        name="email"
-                        label="Email"
-                        variant="outlined"
-                        value={formData.email}
-                        onChange={(e) => handleFormData(e)}
-                        sx={{ width: "90%", marginTop: "10px" }}
-                    />
-                </Typography>
-            </Box>
-            <Box sx={{display: "flex", flexDirection: "row", alignItems: "center"}}>
-                <Typography sx={{textAlign: "right", fontSize: "150%", width: "50%"}}>
-                    Department:
-                </Typography>
-                <Typography sx={{textAlign: "left", marginLeft: "10%", fontSize: "150%", width: "50%"}}>
-                    <TextField
-                        name="department"
-                        label="Department"
-                        variant="outlined"
-                        value={formData.department}
-                        onChange={(e) => handleFormData(e)}
-                        sx={{ width: "90%", marginTop: "10px" }}
-                    />
-                </Typography>
-            </Box>
-            <Box sx={{display: "flex", flexDirection: "row", alignItems: "center"}}>
-                <Typography sx={{textAlign: "right", fontSize: "150%", width: "50%"}}>
-                    Username:
-                </Typography>
-                <Typography sx={{textAlign: "left", marginLeft: "10%", fontSize: "150%", width: "50%"}}>
-                    <TextField
-                        name="username"
-                        label="Username"
-                        variant="outlined"
-                        value={formData.username}
-                        onChange={(e) => handleFormData(e)}
-                        sx={{ width: "90%", marginTop: "10px" }}
-                    />
-                </Typography>
-            </Box>
-            <Box sx={{display: "flex", flexDirection: "row", alignItems: "center"}}>
-                <Typography sx={{textAlign: "right", fontSize: "150%", width: "50%"}}>
-                    Password:
-                </Typography>
-                <Typography sx={{textAlign: "left", marginLeft: "10%", fontSize: "150%", width: "50%"}}>
-                    <TextField
-                        name="password"
-                        label="Password"
-                        variant="outlined"
-                        value={formData.password}
-                        onChange={(e) => handleFormData(e)}
-                        sx={{ width: "90%", marginTop: "10px" }}
-                    />
-                </Typography>
-            </Box>
-            <Box sx={{display: "flex", flexDirection: "row", alignItems: "center"}}>
-                <Typography sx={{textAlign: "right", fontSize: "150%", width: "50%"}}>
-                    Course:
-                    <Typography variant="subtitle1">(Leave blank if not assigning)</Typography>
-                </Typography>
-                <Typography sx={{textAlign: "left", marginLeft: "10%", fontSize: "150%", width: "50%"}}>
-                    <TextField
-                        name="course"
-                        label="Course"
-                        variant="outlined"
-                        value={formData.course}
-                        onChange={(e) => handleFormData(e)}
-                        sx={{ width: "90%", marginTop: "10px" }}
-                    />
-                    {filteredItems.length > 0 && (
-                        <Paper style={{ position: 'absolute', zIndex: 1, width: '100%' }}>
-                        <List>
-                            {filteredItems.map((item, index) => (
-                            <ListItem button key={index} onClick={() => handleItemClick(item)}>
-                                <ListItemText primary={item} />
-                            </ListItem>
-                            ))}
-                        </List>
-                        </Paper>
-                    )}
-                </Typography>
-            </Box>
-            <Box sx={{textAlign: "center", paddingTop: "20px"}}>
-                <Button type="submit" sx={{border: "3px solid black", textAlign: "center", width: "30%", height: "80%", color: "white", backgroundColor: "rgba(255, 127, 50, 0.8)", '&:hover': {backgroundColor: "rgba(255, 127, 50, 1)"}}} >
-                     Submit
-                </Button>
-            </Box>
-            </form>
-        </DialogContent>
-    </Dialog>
-      )
+                <form onSubmit={handleSubmit}>
+                    <Stack spacing={2} sx={{ p: 2 }} divider={<Divider orientation="horizontal" flexItem />}>
+                        {getInput('Name', 'name')}
+                        {getInput('Email', 'email')}
+                        {getInput('Department', 'department')}
+                        {getInput('Username', 'username')}
+                        {getInput('Password', 'password')}
+                        {getInput('Course', 'courses')}
+                    </Stack>
+                    <Box sx={{ textAlign: "center", padding: "20px" }}>
+                        <Button type="submit" variant='contained' color='secondary'>
+                            Submit
+                        </Button>
+                    </Box>
+                </form>
+            </DialogContent>
+        </Dialog>
+    )
 }
 
 export default AddProfessorForm;
