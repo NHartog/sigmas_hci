97062423a73f59135c3a297040459fdb6255048f
Sebastian Paulis <sebastian4paulis@gmail.com>
2024-11-04 19:46:09 -0500

    fix: Repair buttons to be fully horizontally scaling

---
+++ src/app/error.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+import { Typography } from "@mui/material"
+import React from "react"
+
+export default function ErrorPage(){
+    return (
+        <Typography>Please log in before coming to this url</Typography>
+    )
+}
